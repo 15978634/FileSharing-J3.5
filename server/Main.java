ec277be13e4cc9e149a5006e8062a642c37a538e
lukas027 <lukas.weiss27@web.de>
2017-08-28 16:26:49 +0200

    hi

--- server/Main.java
+++ server/Main.java
@@ -1,12 +1,42 @@
 package server;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
 
 public class Main {
 
+	public static final int DEFAULT_PORT = 8887;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("hi");
-		System.out.println("hii");
-		System.out.println("hiii");
+		ServerSocket listener = null;
+		Socket clientSocket = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+		try {
+			listener = new ServerSocket(DEFAULT_PORT);
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+		
+			try {
+				while(true) {
+					clientSocket = listener.accept();
+					if (clientSocket != null) {
+						//while(true) {
+						
+						System.out.println(clientSocket.getInetAddress() + "connected to the Server");
+						out = new PrintWriter(clientSocket.getOutputStream(), true);
+						out.println("lars ist ein lappen");
+						in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+						System.out.println(in.readLine());
+						clientSocket.close();
+						//}
+					}
+				}
+			} catch (IOException e) {
+				System.err.println(e);
+		}
 	}
 
 }
