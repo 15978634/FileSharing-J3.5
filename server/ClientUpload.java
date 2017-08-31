c13ff8d5b370dcbb91b4eccd192705d300d7d70b
lukas027 <lukas.weiss27@web.de>
2017-08-31 10:44:22 +0200

    server upload

--- server/ClientUpload.java
+++ server/ClientUpload.java
@@ -1,6 +1,7 @@
 package server;
 
 import java.io.DataInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -9,11 +10,11 @@ public class ClientUpload implements Runnable {
 	private Socket fileshareSocket;
 	private int fileId;
 	private DataInputStream inputStream;
+	File newFile;
 	public ClientUpload(Socket fileshareSocket, int fileId) {
 		
 		this.fileshareSocket = fileshareSocket;
 		this.fileId = fileId;
-		
 		try {
 			inputStream = new DataInputStream(fileshareSocket.getInputStream());
 		} catch (IOException e) {
@@ -24,7 +25,18 @@ public class ClientUpload implements Runnable {
 	
 	@Override
 	public void run() {
-		
+		try {
+			int length = inputStream.readInt();
+			String name = inputStream.readUTF();
+			byte[] content = new byte[length];
+			inputStream.read(content);
+			newFile = new File(Server.getSourceDirectory() + "" + name);
+			newFile.createNewFile();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
